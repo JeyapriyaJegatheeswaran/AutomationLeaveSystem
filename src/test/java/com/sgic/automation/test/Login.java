a7d0da6cf1e50a395ecd629c2085a0ab670b398e
JeyapriyaJegatheeswaran <jjypriya@gmail.com>
2019-02-07 13:08:41 +0530

    config folder

--- src/test/java/com/sgic/automation/test/Login.java
+++ src/test/java/com/sgic/automation/test/Login.java
@@ -10,54 +10,14 @@
 package com.sgic.automation.test;
 import static org.testng.Assert.assertEquals;
 
-import java.io.File;
-import java.lang.reflect.Method;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
-public class Login {
-	public String baseUrl="http://localhost:4200/";
-	public String driverPath="webdrivers//chromedriver.exe";
-	public WebDriver driver;
+public class Login extends Base{
 	
-	ExtentReports extentReports;
-	ExtentTest extentTest;
-	ExtentHtmlReporter extentHtmlReporter;
-	
-	@BeforeTest
-	public void setUp() {
-
-		extentHtmlReporter = new ExtentHtmlReporter(
-				new File(System.getProperty("user.dir") + "/AutomationReports.html"));
-		extentHtmlReporter.loadXMLConfig(new File(System.getProperty("user.dir") + "/ReportsConfig.xml"));
-		extentReports = new ExtentReports();
-		extentReports.setSystemInfo("Environment", "QA");
-		extentReports.attachReporter(extentHtmlReporter);
-	}
-	
-	@BeforeMethod
-	public void invokeBrowser(Method method) {
-		System.setProperty("webdriver.chrome.driver", driverPath);
-		driver= new ChromeDriver();
-		driver.get(baseUrl);
-		
-		String descriptiveTestName = method.getAnnotation(Test.class).testName();
-		extentTest = extentReports.createTest(descriptiveTestName);
-	}
 //verify login	
   @Test(priority=1,dataProvider="HRMSLogin",testName="test login ")
   public void login(String userName , String password)  {
@@ -86,25 +46,7 @@ public class Login {
 		}
   }
  
-  @AfterMethod
-	public void tearDown(ITestResult result) {
-		if (result.getStatus() == ITestResult.SUCCESS) {
-			extentTest.log(Status.PASS, "The Test Method Name As : " + result.getName() + " is Passed ");
-
-		} else if (result.getStatus() == ITestResult.FAILURE) {
-			extentTest.log(Status.FAIL, "The Test Method Name As : " + result.getName() + " is Fail ");
-			extentTest.log(Status.FAIL, "The Test Failure : " + result.getThrowable());
-		} else if (result.getStatus() == ITestResult.SKIP) {
-			extentTest.log(Status.SKIP, "The Test Method Name As : " + result.getName() + " is Skiped ");
-		}
-		extentReports.flush();
-		driver.quit();
-	}
   
-  @AfterTest
-	public void endReport() {
-		extentReports.flush();
-	}
   
   @DataProvider(name = "HRMSLogin")
 	public Object[][] HRMLogin() {
